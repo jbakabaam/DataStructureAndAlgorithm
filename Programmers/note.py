39474802aaa81d6af67253deac51e1600e058a4e
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-11-08 16:40:34 +0900

    Update note.py

--- Programmers/note.py
+++ Programmers/note.py
@@ -4,7 +4,7 @@ def solution(x):
     return answer
 
 
-# 2-1.
+# 2-1. Linear Array
 L = [20, 37, 58, 72, 91]
 def solution(L, x):
     L.append(x)
@@ -21,10 +21,30 @@ def solution(L, x):
     return answer
 
   
-# 2-2. 
+# 2-2. Linear Array
 L = [64, 72, 83, 72, 54]
 def solution(L, x):
     if x in L:
         return [i for i, y in enumerate(L) if y == x]
     else:
         return [-1]
+
+
+# 3. Binary Search
+L = [2,3,5,6,9,11,15]
+def solution(L, x):
+    lower = 0
+    upper = len(L) - 1
+    idx = -1
+    
+    while lower <= upper:
+        middle = (lower + upper) // 2
+        if L[middle] == x:
+            idx = middle
+            break
+        elif L[middle] < x:
+            lower = middle + 1
+        else:
+            upper = middle - 1
+            
+    return idx
